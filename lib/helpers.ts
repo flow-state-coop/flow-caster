9bfaa33e80fabb347878bdf254bd3e322ce7a806
skuhlmann <samkuhlmann@gmail.com>
2025-11-11 14:46:56 -0700

    fixes explorer link

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,7 +2,8 @@ import sdk from "@farcaster/miniapp-sdk";
 import { FEATURED_POOL_NETWORK } from "./constants";
 
 export const openExplorerUrl = (hash: string) => {
-  sdk.actions.openUrl(`${FEATURED_POOL_NETWORK}tx/${hash}`);
+  if (!FEATURED_POOL_NETWORK) return;
+  sdk.actions.openUrl(`${FEATURED_POOL_NETWORK.blockExplorer}tx/${hash}`);
 };
 
 export const openUrl = (url: string) => {
